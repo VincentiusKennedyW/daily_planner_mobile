d60bfb74470104a7b9a03aab494b21eaef3ac334
VincentiusKennedyW <kenken.kk40@gmail.com>
2025-07-07 16:22:32 +0800

    refactor: replace TaskListWidget with buildTaskList function for reactive

--- lib/app/modules/daily_planner/views/task_list/widgets/taks_list.dart
+++ lib/app/modules/daily_planner/views/task_list/widgets/taks_list.dart
@@ -7,74 +7,59 @@ import 'package:expense_tracker/app/modules/daily_planner/views/task_list/widget
 import 'package:expense_tracker/app/modules/daily_planner/views/task_list/widgets/task_list_bottom_loader.dart';
 import 'package:expense_tracker/core/task.dart';
 
-class TaskListWidget extends StatelessWidget {
-  final List<TaskListModel> tasks;
-  final bool isLoading;
-  final bool isLoadMore;
-  final String errorMessage;
-  final VoidCallback onRefresh;
-  final ScrollController scrollController;
-  final TaskStatus status;
+Widget buildTaskList(
+  List<TaskListModel> tasks,
+  bool isLoading,
+  bool isLoadMore,
+  String errorMessage,
+  VoidCallback onRefresh,
+  ScrollController scrollController,
+  TaskStatus status,
+) {
+  if (isLoading) {
+    return Center(child: CircularProgressIndicator());
+  }
 
-  const TaskListWidget({
-    super.key,
-    required this.tasks,
-    required this.isLoading,
-    required this.isLoadMore,
-    required this.errorMessage,
-    required this.onRefresh,
-    required this.scrollController,
-    required this.status,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    if (isLoading) {
-      return Center(child: CircularProgressIndicator());
-    }
-
-    if (errorMessage.isNotEmpty) {
-      return ProjectOrTaskErrorView(
-          isTaskError: true,
-          errorMessage: errorMessage,
-          onRetry: () {
-            onRefresh();
-          });
-    }
-
-    if (tasks.isEmpty) {
-      return EmptyTaskOrProjectView(
-        isTaskView: true,
-      );
-    }
-
-    return Column(
-      children: [
-        // Informasi pagination di bagian atas
-        // Obx(() => PaginationInfo(
-        //       pagination: _getPaginationByStatus(status),
-        //       status: status,
-        //     )),
-        Expanded(
-          child: RefreshIndicator(
-            onRefresh: () async => onRefresh(),
-            child: ListView.builder(
-              controller: scrollController,
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              itemCount: tasks.length + 1,
-              itemBuilder: (context, index) {
-                if (index < tasks.length) {
-                  return TaskCard(
-                    task: tasks[index],
-                  );
-                } else {
-                  return buildBottomLoader(isLoadMore, status);
-                }
-              },
-            ),
-          ),
-        ),
-      ],
+  if (errorMessage.isNotEmpty) {
+    return ProjectOrTaskErrorView(
+      isTaskError: true,
+      errorMessage: errorMessage,
+      onRetry: onRefresh,
     );
   }
+
+  if (tasks.isEmpty) {
+    return EmptyTaskOrProjectView(
+      isTaskView: true,
+    );
+  }
+
+  return Column(
+    children: [
+      // Informasi pagination di bagian atas
+      // Obx(() => PaginationInfo(
+      //       pagination: _getPaginationByStatus(status),
+      //       status: status,
+      //     )),
+      Expanded(
+        child: RefreshIndicator(
+          onRefresh: () async => onRefresh(),
+          child: ListView.builder(
+            controller: scrollController,
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            itemCount: tasks.length + 1,
+            itemBuilder: (context, index) {
+              if (index < tasks.length) {
+                return TaskCard(
+                  task: tasks[index],
+                );
+              } else {
+                return buildBottomLoader(isLoadMore, status);
+              }
+            },
+          ),
+        ),
+      ),
+    ],
+  );
 }
